2b005f0cbe24579231d295f8a39e0860a53da0dc
ニコワン（Niko'one） <153420772+Nikonosfive@users.noreply.github.com>
2025-12-01 00:15:54 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // 自分のGitHubアカウント名とリポジトリ名に合わせて書き換えてください
+  site: 'https://nikonosfive.github.io',
+  base: '/Geodic',
+});
