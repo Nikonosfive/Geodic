11f6cd1732af972323b629d80f7f4abd5d00c9ae
ニコワン（Niko'one） <153420772+Nikonosfive@users.noreply.github.com>
2025-12-01 09:37:35 +0000

    試作ページ作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,35 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://nikonosfive.github.io",
-	base: isProd ? '/Geodic' : '/',
-	integrations: [
-		starlight({
-			title: '地理の扉',
-			defaultLocale: 'root',
-			locales: {
-				root: {
-					label: '日本語',
-					lang: 'ja',
-				},
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nikonosfive' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: '地理資料集',
-					autogenerate: { directory: 'material' },
-				},
-				{
-					label: '産業',
-					autogenerate: { directory: 'material/industry' },
-				},
-			],
-		}),
+  site: "https://nikonosfive.github.io",
+  base: isProd ? '/Geodic' : '/',
+
+  integrations: [
+      starlight({
+          title: '地理の扉',
+          defaultLocale: 'root',
+          locales: {
+              root: {
+                  label: '日本語',
+                  lang: 'ja',
+              },
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nikonosfive' }],
+          sidebar: [
+              {
+                  label: '地理資料集',
+                  autogenerate: { directory: 'material' },
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
