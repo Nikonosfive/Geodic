f440898856aa2c9044a08396738d866d78007678
ニコワン（Niko'one） <153420772+Nikonosfive@users.noreply.github.com>
2025-12-01 01:48:39 +0000

    テンプレ変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,35 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  // 自分のGitHubアカウント名とリポジトリ名に合わせて書き換えてください
-  site: 'https://nikonosfive.github.io',
-  base: '/Geodic',
+	site: "https://nikonosfive.github.io",
+	base: "/Geodic",
+	integrations: [
+		starlight({
+			title: '地理の扉',
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: '日本語',
+					lang: 'ja',
+				},
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nikonosfive' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
