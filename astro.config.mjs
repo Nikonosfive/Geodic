d384c438a56ead41098f6f8ce1429516f4ea464c
ニコワン（Niko'one） <153420772+Nikonosfive@users.noreply.github.com>
2025-12-01 04:19:43 +0000

    微修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://nikonosfive.github.io",
-	base: "/Geodic",
+	base: isProd ? '/Geodic' : '/',
 	integrations: [
 		starlight({
 			title: '地理の扉',
